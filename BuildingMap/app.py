1258bfd9962af9cda4b9538223d7ae0f8c1e06e1
niklasfugledal <niklasfugledal@live.no>
2023-03-02 13:23:00 +0100

    Flask-test-project, not finished

--- BuildingMap/app.py
+++ BuildingMap/app.py
@@ -1,35 +1,53 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buildings.db'
+# PostgreSQL database connection details
+host = "postgresql-dev-kartai.postgres.database.azure.com"
+port = "5432"
+database = "kartai_bachelor_2023"
+user = "kartai_bachelor_2023@postgresql-dev-kartai"
+password = "Io4$7M1e"
+
+
+
+# Configure app to use PostgreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route('/add_building', methods=['POST'])
-def add_building():
-    name = request.form['name']
-    address = request.form['address']
-    latitude = float(request.form['latitude'])
-    longitude = float(request.form['longitude'])
-    building = Building(name=name, address=address, latitude=latitude, longitude=longitude)
-    db.session.add(building)
-    db.session.commit()
-    return jsonify({'result': 'success'})
 
 class Building(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-name = db.Column(db.String(100))
-address = db.Column(db.String(200))
-latitude = db.Column(db.Float)
-longitude = db.Column(db.Float)
-   
+    name = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
 
-    
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/save-building', methods=['POST'])
+def save_building():
+    name = request.form['name']
+    address = request.form['address']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+
+    # Insert data into the database
+    try:
+        building = Building(name=name, address=address, latitude=latitude, longitude=longitude)
+        db.session.add(building)
+        db.session.commit()
+        return jsonify({'success': True, 'message': 'Building saved successfully'})
+    except Exception as e:
+        print(e)
+        return jsonify({'success': False, 'message': 'Failed to save building'})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-  
--- BuildingMap/db.py
+++ BuildingMap/db.py
@@ -1,36 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
-# Database connection details
+app = Flask(__name__)
+
+# PostgreSQL database connection details
 host = "postgresql-dev-kartai.postgres.database.azure.com"
 port = "5432"
 database = "kartai_bachelor_2023"
 user = "kartai_bachelor_2023@postgresql-dev-kartai"
 password = "Io4$7M1e"
 
-# Connect to PostgreSQL database
-conn = psycopg2.connect(
-    host=host,
-    port=port,
-    database=database,
-    user=user,
-    password=password
-)
+# Configure app to use PostgreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# Create a cursor object
-cur = conn.cursor()
 
-# Create buildings table
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS buildings (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        address VARCHAR(255) NOT NULL,
-        latitude FLOAT NOT NULL,
-        longitude FLOAT NOT NULL
-    )
-""")
+class Building(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
 
-# Commit changes and close cursor and connection
-conn.commit()
-cur.close()
-conn.close()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/save-building', methods=['POST'])
+def save_building():
+    name = request.form['name']
+    address = request.form['address']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+
+    # Insert data into the database
+    try:
+        building = Building(name=name, address=address, latitude=latitude, longitude=longitude)
+        db.session.add(building)
+        db.session.commit()
+        return jsonify({'success': True, 'message': 'Building saved successfully'})
+    except Exception as e:
+        print(e)
+        return jsonify({'success': False, 'message': 'Failed to save building'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
